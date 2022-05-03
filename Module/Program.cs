2faadce7f0d36bdad4202837a1b64064d7488ca4
Lashkin <liahim9719@gmail.com>
2022-05-03 17:13:29 +0300

    Задание код Застрял на пункте "Значения ячеек этих массивов равны порядковому номеру каждой буквы в алфавите."

--- Module/Program.cs
+++ Module/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace Module
 {
@@ -6,7 +7,31 @@ namespace Module
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int a;
+            Console.WriteLine("Enter origin array size");
+            a = Convert.ToInt32(Console.ReadLine());
+            int[] originAr = new int[a];
+            Random rand = new Random();
+
+            for (int i = 0; i < originAr.Length; i++)
+            {
+                originAr[i] = rand.Next(1, 26);
+            }
+
+            int[] positive = originAr.Where(n => n % 2 == 0).ToArray();
+            int[] negative = originAr.Where(n => n % 2 != 0).ToArray();
+            object[] chaerPositive = new object[positive.Length];
+            object[] chaerNegative = new object[negative.Length];
+            Array.Copy(positive, chaerPositive, positive.Length);
+            Array.Copy(negative, chaerNegative, negative.Length);
+
+            for (int i = 0; i < chaerPositive.Length; i++)
+            {
+                chaerPositive[i] = Convert.ToChar(rand.Next('a', 'z'));
+                chaerNegative[i] = Convert.ToChar(rand.Next('a', 'z'));
+            }
+            Console.WriteLine((string.Join(" ", chaerPositive)));
+            Console.WriteLine((string.Join(" ", chaerNegative)));
         }
     }
 }
